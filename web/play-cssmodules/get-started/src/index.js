d2c3095df0507ed2a9d70ac451b7603ae19933df
samupanz18 <samupanz18@gmail.com>
2017-05-27 13:05:33 +0800

    Finish the get-started project of play-cssmodules

--- web/play-cssmodules/get-started/src/index.js
+++ web/play-cssmodules/get-started/src/index.js
@@ -1,1 +1,9 @@
-require('./alert.js');
+import styles from './app.css';
+
+let element = `
+  <div class="${styles.element}">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</p>
+  </div>
+`
+
+document.write(element);
--- web/play-cssmodules/get-started/webpack.config.js
+++ web/play-cssmodules/get-started/webpack.config.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const PATHS = {
+    src: path.join(__dirname, 'src'),
     build: path.join(__dirname, 'build'),
 };
 
 module.exports = {
-    entry: './src',
+    entry: PATHS.src,
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        loaders: [{
+            test: /\.js/,
+            loader: 'babel-loader',
+            include: PATHS.src
+        }, {
+            test: /\.css/,
+            loader: ExtractTextPlugin.extract("css-loader?modules&importLoaers=1&localIdentName=[name]__[local]___[hash:base64:5]"),
+            include: PATHS.src
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ]
 };
