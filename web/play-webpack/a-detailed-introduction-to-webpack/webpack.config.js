e0cff02c113b590392e689c372945bada4962cf3
samuel.gong <samuel.gong@ringcentral.com>
2017-08-04 16:36:28 +0800

    Replace express with webpack-dev-server

--- web/play-webpack/a-detailed-introduction-to-webpack/webpack.config.js
+++ web/play-webpack/a-detailed-introduction-to-webpack/webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
     module: {
         rules: [
             {
